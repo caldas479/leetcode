54e27ae89f907fbe5b81bdeafd264eb0eb4f6edf
Tiago Rodrigues Caldas <94724871+caldas479@users.noreply.github.com>
2024-03-12 17:29:55 +0000

    Update removeZeroSumSublists.java

--- daily/removeZeroSumSublists.java
+++ daily/removeZeroSumSublists.java
@@ -9,6 +9,10 @@
  * }
  */
 class Solution {
+    /**
+    * Given the head of a linked list, we repeatedly delete consecutive sequences of nodes that sum to 0 until there are no such sequences.
+    * After doing so, return the head of the final linked list.  You may return any such answer.
+    */
     public ListNode removeZeroSumSublists(ListNode head) {
 
         if (head == null) {
